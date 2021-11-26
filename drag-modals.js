e6dcf0bf84d2c7aaac6abc738569445bdc864aa5
tibo35 <54692607+tibo35@users.noreply.github.com>
2021-11-26 16:10:46 +1000

    mousedown modals

--- drag-modals.js
+++ drag-modals.js
@@ -31,3 +31,56 @@ function mousedown(e){
 }
 
 });
+
+
+// Resize
+
+// const el = document.querySelector('.resize')
+// const resizers = document.querySelectorAll(".resizer");
+// let currentResizer;
+
+// for (let resizer of resizers) {
+//   resizer.addEventListener("mousedown", mousedown);
+
+//   function mousedown(e) {
+//     currentResizer = e.target;
+//     isResizing = true;
+
+//     let prevX = e.clientX;
+//     let prevY = e.clientY;
+
+//     window.addEventListener("mousemove", mousemove);
+//     window.addEventListener("mouseup", mouseup);
+
+//     function mousemove(e) {
+//       const rect = el.getBoundingClientRect();
+
+//       if (currentResizer.classList.contains("se")) {
+//         el.style.width = rect.width - (prevX - e.clientX) + "px";
+//         el.style.height = rect.height - (prevY - e.clientY) + "px";
+//       } else if (currentResizer.classList.contains("sw")) {
+//         el.style.width = rect.width + (prevX - e.clientX) + "px";
+//         el.style.height = rect.height - (prevY - e.clientY) + "px";
+//         el.style.left = rect.left - (prevX - e.clientX) + "px";
+//       } else if (currentResizer.classList.contains("ne")) {
+//         el.style.width = rect.width - (prevX - e.clientX) + "px";
+//         el.style.height = rect.height + (prevY - e.clientY) + "px";
+//         el.style.top = rect.top - (prevY - e.clientY) + "px";
+//       } else {
+//         el.style.width = rect.width + (prevX - e.clientX) + "px";
+//         el.style.height = rect.height + (prevY - e.clientY) + "px";
+//         el.style.top = rect.top - (prevY - e.clientY) + "px";
+//         el.style.left = rect.left - (prevX - e.clientX) + "px";
+//       }
+
+//       prevX = e.clientX;
+//       prevY = e.clientY;
+//     }
+
+//     function mouseup() {
+//       window.removeEventListener("mousemove", mousemove);
+//       window.removeEventListener("mouseup", mouseup);
+//       isResizing = false;
+//     }
+//   }
+// }
