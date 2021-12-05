d708ac040bc7f04a62f999b2563bcfdbef70000a
tibo35 <54692607+tibo35@users.noreply.github.com>
2021-12-05 21:47:59 +1000

    feature work-modal, JS, change the content in the same modal. Style: button nav CSS

--- text.js
+++ text.js
@@ -2,36 +2,36 @@
 
 // ===========================  TIME  ========================
 function currentTime() {
-    let date = new Date(); 
-    let hh = date.getHours();
-    let mm = date.getMinutes();
-    // let ss = date.getSeconds();
-    let session = "AM";
-  
-    if(hh == 0){
-        hh = 12;
-    }
-    if(hh > 12){
-        hh = hh - 12;
-        session = "PM";
-     }
-  
-     hh = (hh < 10) ? "0" + hh : hh;
-     mm = (mm < 10) ? "0" + mm : mm;
-    //  ss = (ss < 10) ? "0" + ss : ss;
-      
-     let time = hh + ":" + mm + " " + session;
-  
-    document.getElementById("clock").innerText = time; 
-    let t = setTimeout(function(){ currentTime() }, 1000);
+  let date = new Date(); 
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  // let ss = date.getSeconds();
+  let session = "AM";
+
+  if(hh == 0){
+      hh = 12;
   }
-  currentTime();
+  if(hh > 12){
+      hh = hh - 12;
+      session = "PM";
+   }
+
+   hh = (hh < 10) ? "0" + hh : hh;
+   mm = (mm < 10) ? "0" + mm : mm;
+  //  ss = (ss < 10) ? "0" + ss : ss;
+    
+   let time = hh + ":" + mm + " " + session;
+
+  document.getElementById("clock").innerText = time; 
+  let t = setTimeout(function(){ currentTime() }, 1000);
+}
+currentTime();
 
 
-   
-  // ============================ DATE  =======================
+ 
+// ============================ DATE  =======================
 
-  function currentDate() {
+function currentDate() {
 var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -39,8 +39,8 @@ var yyyy = today.getFullYear();
 
 today = mm + ' / ' + dd + ' / ' + yyyy;
 document.getElementById("date").innerText = today; 
-  }
-  currentDate();
+}
+currentDate();
 
 
 
@@ -53,102 +53,171 @@ document.getElementById("date").innerText = today;
 
 
 
-// ================     BUTTONS     =============
+// ================     Modal-windows     =============
 
 
 const triggers = document.getElementsByClassName('trigger');
 const modals = document.getElementsByClassName('modal');
 
-
 const closeButtons = document.getElementsByClassName('dot-close');
 const expandButtons = document.getElementsByClassName('dot-open');
 const reduceButtons = document.getElementsByClassName('dot-reduce');
-
-const expandArray = Array.from(expandButtons).entries();
-const triggerArray = Array.from(triggers).entries();
-const reduceArray = Array.from(reduceButtons).entries();
-
-
-
-
-
-for (let [i, trigger] of triggerArray){
-
-// Open the modal widow
-
-  const showModal = () => {
-   
-    modals[i].classList.add('show-modal');
-    modals[i].classList.remove('modal-expand');
-
-  }
-  trigger.addEventListener('dblclick', showModal);
+const soba = document.getElementById('soba');
+const work = document.getElementById('work');
+const sobaReadMe = document.getElementById('soba-readme');
+const work2 = document.getElementById('work2');
+const work2ReadMe = document.getElementById('work2-readMe');
+const work3 = document.getElementById('work3');
+const work3ReadMe = document.getElementById('work3-readMe');
 
 
+for (let i=0; i < triggers.length; i++){
 
-// Close the modal widow
-  const removeModal = () => {
-    modals[i].classList.remove('modal-expand');
-    modals[i].classList.remove('show-modal');
-   
-  }
-  closeButtons[i].addEventListener('click', removeModal);
-  reduceButtons[i].addEventListener('click', removeModal);
+// Open the modal widows
+triggers[i].addEventListener('dblclick', showModal);
 
+// Close the modal widows
+closeButtons[i].addEventListener('click', removeModal);
 
- 
+// Minimize the modal windows
+reduceButtons[i].addEventListener('click', removeModal);
 
 // Bring modal widow at the front (zindex)
+modals[i].addEventListener("click", bringFront);
+modals[i].addEventListener("mousedown", bringFront);
 
-var zIndex =10;
-  const bringFront = () => {
+// keep modal window stays at the front when open:
+triggers[i].addEventListener('dblclick', bringFront);
+
+// expand.addEventListener('click', expandModal);
+expandButtons[i].addEventListener('click', expandModal);
+
+
+// ====================   FUNCTIONS =================
+
+
+
+function showModal(){
+  modals[i].classList.add('show-modal');
+  modals[i].classList.remove('modal-expand');
+
+  // if else statment for the Work modal. if <child> is opening <parent> removed:
+
+
+soba.addEventListener('mouseover', removeWork)
+sobaReadMe.addEventListener('mouseover', removeSoba)
+work2.addEventListener('mouseover', removeWork)
+work2ReadMe.addEventListener('mouseover', removeWork2)
+work3.addEventListener('mouseover', removeWork)
+work3ReadMe.addEventListener('mouseover', removeWork3)
+
+
+function removeWork(){
+      work.style.display='none';
+}
+function removeSoba(){
+  soba.style.display='none';
+}
+function removeWork2 (){
+  work2.style.display='none'
+}
+function removeWork3 (){
+  work3.style.display='none'
+}
+}
+
+
+
+
+function removeModal() {
+  modals[i].classList.remove('modal-expand');
+  modals[i].classList.remove('show-modal');
+};
+
+ var zIndex =10;
+function bringFront(){
+  modals[i].style.zIndex = zIndex++;
+};
+
+function expandModal() {
+  modals[i].classList.toggle('modal-expand');
+};
+
+};
+
+// ================     Switch box-modal WORK    =============
+// const container = document.getElementsByClassName('container')
+// const workFolder = document.querySelector('.modal-box-work')
+// const sobaFolder = document.querySelector('.modal-box-sobaContainer')
+// const triggerSobaFolder = document.getElementById('item-work1-trigger')
+// const triggerWork2Folder = document.getElementById('work2-folder-trigger')
+// const modalBox = document.querySelectorAll('.modal-box')
+// const modal = document.querySelectorAll('.modal')
+
+
+
+//   triggerSobaFolder.addEventListener('dblclick', openSobaFolder)
+//   // triggerWork2Folder.addEventListener('dbclick', openWork2Folder)
+
+//   function openSobaFolder(){
+//   workFolder.classList.toggle('hide-work-folder')
+// sobaFolder.classList.toggle('show-soba')
+  
+    // sobaFolder[i].classList.add('container-soba-folder')
+    // sobaFolder[i].style.visibility='visible';
     
-    modals[i].style.zIndex = zIndex++;
-  }
-  modals[i].addEventListener("click", bringFront);
-
-  modals[i].addEventListener("mousedown", bringFront);
-
-
-  // make sure that the modal window stays at the front when open:
-  trigger.addEventListener('dblclick', bringFront);
-  }
   
 
+// const workFolder = document.getElementById('work');
+// const triggerBox = document.getElementsByClassName('trigger-box');
+// const modalBox = document.getElementsByClassName('modal-box');
+// const triggerSoba = document.getElementById('item-work1-trigger');
+// const containerSoba = document.getElementById('soba-container');
 
-for(let [i, expand] of expandArray) {
+// for(let i=0; i < modalBox.length; i++){
+// triggerSoba.addEventListener('dblclick', openSobaContainer)
 
-// Expand the modal widow
+// function openSobaContainer(){
+// workFolder.style.visibility='hidden'
+// containerSoba.style.visibility="visible"
+// return
 
-  const expandModal = () => {
-    modals[i].classList.toggle('modal-expand');
-    
-  }
-  // expand.addEventListener('click', expandModal);
-  expandButtons[i].addEventListener('click', expandModal);
-
-}
-
-const nextBtn = document.querySelectorAll('.next-btn')
-const work = document.querySelectorAll('work')
-const soba = document.querySelectorAll('soba')
-const backBtn = document.querySelectorAll('.back-btn')
-const sobaFolder = document.querySelectorAll('.modal > .container-soba-folder') 
-const modalbox = document.querySelectorAll('.modal-box');
-const backArray = Array.from(backBtn).entries()
-const nextArray = Array.from(nextBtn).entries()
+// }
 
 
-for(let [i, back] of backArray){
-// BACK BUTTONS 
-const remove = () => {
 
-  document.getElementById('soba').classList.remove('show-modal')
-  return
-  // e[i].classList.add('container-work-folder')
-}
-backBtn[i].addEventListener('click', remove);
-}
+
+// const triggerBox = document.getElementsByClassName('trigger-box');
+// const modalBox = document.getElementsByClassName('modal-box');
+
+
+// const triggerBox = Array.from(triggerBox).entries();
+
+// for (let [i, trigger] of triggerBox){
+// trigger.addEventListener('dblclick')
+
+// }
+
+// const nextBtn = document.querySelectorAll('.next-btn')
+// const work = document.querySelectorAll('work')
+// const soba = document.querySelectorAll('soba')
+// const backBtn = document.querySelectorAll('.back-btn')
+// const sobaFolder = document.querySelectorAll('.modal > .container-soba-folder') 
+// const modalbox = document.querySelectorAll('.modal-box');
+// const backArray = Array.from(backBtn).entries()
+// const nextArray = Array.from(nextBtn).entries()
+
+
+// for(let [i, back] of backArray){
+// // BACK BUTTONS 
+// const remove = () => {
+
+// document.getElementById('soba').classList.remove('show-modal')
+// return
+// // e[i].classList.add('container-work-folder')
+// }
+// backBtn[i].addEventListener('click', remove);
+// }
 
 // for(let [i, next] of nextArray){
 // const next = () => {
@@ -176,31 +245,31 @@ let isResizing = false;
 el.addEventListener("mousedown", mousedown);
 
 function mousedown(e) {
-  window.addEventListener("mousemove", mousemove);
-  window.addEventListener("mouseup", mouseup);
+window.addEventListener("mousemove", mousemove);
+window.addEventListener("mouseup", mouseup);
 
-  let prevX = e.clientX;
-  let prevY = e.clientY;
+let prevX = e.clientX;
+let prevY = e.clientY;
 
-  function mousemove(e) {
-    if (!isResizing) {
-      let newX = prevX - e.clientX;
-      let newY = prevY - e.clientY;
+function mousemove(e) {
+  if (!isResizing) {
+    let newX = prevX - e.clientX;
+    let newY = prevY - e.clientY;
 
-      const rect = el.getBoundingClientRect();
+    const rect = el.getBoundingClientRect();
 
-      el.style.left = rect.left - newX + "px";
-      el.style.top = rect.top - newY + "px";
+    el.style.left = rect.left - newX + "px";
+    el.style.top = rect.top - newY + "px";
 
-      prevX = e.clientX;
-      prevY = e.clientY;
-    }
+    prevX = e.clientX;
+    prevY = e.clientY;
   }
+}
 
-  function mouseup() {
-    window.removeEventListener("mousemove", mousemove);
-    window.removeEventListener("mouseup", mouseup);
-  }
+function mouseup() {
+  window.removeEventListener("mousemove", mousemove);
+  window.removeEventListener("mouseup", mouseup);
+}
 }
 }
 
@@ -225,9 +294,9 @@ function mousedown(e) {
 //     window.addEventListener("mouseup", mouseup);
 
 //     function mousemove(e) {
-    
+  
 //       let el = currentResizer.closest(".resizable");
-   
+ 
 //       const rect = el.getBoundingClientRect();
 
 //       if (currentResizer.classList.contains("se")) {
@@ -259,7 +328,4 @@ function mousedown(e) {
 //     }
 //   }
 // }
-
-
-
 
